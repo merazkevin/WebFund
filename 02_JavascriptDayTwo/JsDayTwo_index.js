89923ebf0a2764ff52fb3d2a2f98f8662156d582
merazkevin <merazkevin@yahoo.com>
2022-08-16 08:16:50 -0800

    added tuesday

---
+++ 02_JavascriptDayTwo/JsDayTwo_index.js
@@ -0,0 +1,19 @@
+var amazingShow1 = "" //this is a variable that is equal to a string.
+var magicsNumber = 32.01; // 32 is a number
+
+amazingShow1 = "The Office"; //this is an exampleof reasigning the variable
+var isRobSpeakingFactsAboutBasketball = false; //booleans are another data type: true or false.
+//a data type is a simple piece of information such as a string number boolean
+
+//a data structure is something that can store multipledata types.
+
+var greatesArtist = ["drake","J.cole","Kendrick","Kanye"]
+
+var randomNums = [54,1,2,3,4]
+
+let mixedArray = ["potato",5,false,true,"hello",9];//this is a mixedarray-mixed data type.
+
+
+for(var i = 0; i<=100; i++){
+    //this is the body of the for loop
+}console.log(i) 
