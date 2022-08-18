1a7bf4a71c5857352634733b1ff43cdb81f58c80
merazkevin <merazkevin@yahoo.com>
2022-08-18 12:19:41 -0800

    update

---
+++ 02_Javascript/dayFour.js
@@ -0,0 +1,14 @@
+
+/*let x = 3
+
+console.log(x)*/
+
+
+/*function addNums(num1,num2){
+    console.log(num1+num2)
+} 
+let newNum = addNums(5,2) + 100
+console.log(newNum)*/
+
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
