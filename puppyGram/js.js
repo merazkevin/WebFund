8f4f5f3f7f9713389c870d53e0f5fd845559c7b6
merazkevin <merazkevin@yahoo.com>
2022-08-27 17:32:20 -0800

    update

---
+++ puppyGram/js.js
@@ -0,0 +1,12 @@
+// // complete the following function
+// function flatten(arr2d) {
+//     var flat = [];
+//     // your code here
+//     return flat;
+// }
+    
+// var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+// console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
+
+let i = 2
+console.log(i);
